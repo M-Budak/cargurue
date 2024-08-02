c44700916e18c0bc09263954c3cdb64c6ac5fe36
M. Budak <mbudak0079@gmail.com>
2024-08-02 21:52:17 +0300

    "cargurue/ eski haline turn

--- fetch/header.js
+++ fetch/header.js
@@ -2,15 +2,15 @@ class MyHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <header class="bg-dark text-light py-2">
-                <a href="cargurue/anasayfa.html">
+                <a href="/anasayfa.html">
                     <h1 class="logo m-2">aracyorumu.com</h1>
                 </a>
                 <nav class="menu">
                     <ul>
-                        <li><a href="cargurue/markalar.html">Markalar</a></li>
-                        <li><a href="cargurue/kasatipleri.html">Kasalar</a></li>
-                        <li><a href="cargurue/modeller.html">Modeller</a></li>
-                        <li><a href="cargurue/haberler.html">Haberler</a></li>
+                        <li><a href="/markalar.html">Markalar</a></li>
+                        <li><a href="/kasatipleri.html">Kasalar</a></li>
+                        <li><a href="/modeller.html">Modeller</a></li>
+                        <li><a href="/haberler.html">Haberler</a></li>
                     </ul>
                 </nav>
             </header>
