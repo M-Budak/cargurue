25c80edd6c381108123b00650b9ce63f0cbaaaf2
M. Budak <mbudak0079@gmail.com>
2024-08-03 19:13:55 +0300

    footer-chnged

--- fetch/footer.js
+++ fetch/footer.js
@@ -2,84 +2,44 @@ class MyFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <style>
-                .footer_haberler .col-12 li{
-                    margin-left: auto; /* Sağa yasla */
-                    margin-right: 0;   /* Sağ kenara yapışık olmasını sağla */
+                .footer_haberler .col-12 li {
+                    margin-left: auto;
+                    margin-right: 0;
                     width: 125%;
                 }
                 
-                /*footer*/
                 .column-title {
                     text-align: center;
                     margin-bottom: 1rem;
                 }
                 
                 .fa {
-                  padding: 20px;
-                  font-size: 30px;
-                  width: 70px;
-                  text-align: center;
-                  text-decoration: none;
-                  margin: 5px 2px;
+                    padding: 20px;
+                    font-size: 30px;
+                    width: 70px;
+                    text-align: center;
+                    text-decoration: none;
+                    margin: 5px 2px;
                 }
                 
                 .fa:hover {
                     opacity: 0.7;
                 }
                 
-                .fa-facebook {
-                  background: #3B5998;
-                  color: white;
-                }
+                .fa-facebook { background: #3B5998; color: white; }
+                .fa-twitter { background: #55ACEE; color: white; }
+                .fa-google { background: #dd4b39; color: white; }
+                .fa-linkedin { background: #007bb5; color: white; }
+                .fa-youtube { background: #bb0000; color: white; }
+                .fa-instagram { background: #125688; color: white; }
                 
-                .fa-twitter {
-                  background: #55ACEE;
-                  color: white;
-                }
-                
-                .fa-google {
-                  background: #dd4b39;
-                  color: white;
-                }
-                
-                .fa-linkedin {
-                  background: #007bb5;
-                  color: white;
-                }
-                
-                .fa-youtube {
-                  background: #bb0000;
-                  color: white;
-                }
-                
-                .fa-instagram {
-                  background: #125688;
-                  color: white;
-                }
-                
-                  footer h5 {
-                    color: #ffc107;
-                  }
-                
-                  footer ul {
-                    list-style: none;
-                    padding: 0;
-                  }
-                
-                  footer ul li {
-                    margin-bottom: 8px;
-                  }
-                
-                  footer .d-flex a {
-                    font-size: 1.5rem;
-                  }
-                
-                /* HEADER < 769 STARTS  */
+                footer h5 { color: #ffc107; }
+                footer ul { list-style: none; padding: 0; }
+                footer ul li { margin-bottom: 8px; }
+                footer .d-flex a { font-size: 1.5rem; }
                 
                 @media only screen and (max-width: 769px) {
-                
-                .footer_haberler .col-12 li{
-                    width: 100%;
+                    .footer_haberler .col-12 li { width: 100%; }
                 }
             </style>
             <footer class="bg-dark text-light py-4 mt-5">
@@ -103,7 +63,7 @@ class MyFooter extends HTMLElement {
                                         <li><a href="https://m-budak.github.io/cargurue/anasayfa.html">Anasayfa</a></li>
                                         <li><a href="https://m-budak.github.io/cargurue/markalar.html">Markalar</a></li>
                                         <li><a href="https://m-budak.github.io/cargurue/kasa.html">Kasalar</a></li>
-                                        <li><a href="https://m-budak.github.io/cargurue/model.html">Modeller</a></li>
+                                        <li><a href="https://m-budak.github.io/cargurue/modeller.html">Modeller</a></li>
                                         <li><a href="https://m-budak.github.io/cargurue/haberler.html">Haberler</a></li>
                                         <li><a href="https://m-budak.github.io/cargurue/hakkimizda.html">Hakkımızda</a></li>
                                     </div>
@@ -120,7 +80,7 @@ class MyFooter extends HTMLElement {
                         </div>
                         
                         <div class="col-sm-12 col-md-2 mt-3">
-                            <a href="https://m-budak.github.io/cargurue/model.html">
+                            <a href="https://m-budak.github.io/cargurue/modeller.html">
                                 <h5 class="column-title">Modeller</h5>
                             </a>
                             <ul class="list-unstyled">
@@ -147,8 +107,8 @@ class MyFooter extends HTMLElement {
                         
                         <div class="col-sm-12 col-md-2 mt-3">
                             <a href="https://m-budak.github.io/cargurue/markalar.html">
-                            <h5 class="column-title">Markalar</h5>
-                          </a>
+                                <h5 class="column-title">Markalar</h5>
+                            </a>
                             <ul class="list-unstyled">
                                 <div class="row">
                                     <div class="col-6 col-md-12">
@@ -173,17 +133,16 @@ class MyFooter extends HTMLElement {
               
                         <div class="col-sm-12 col-md-2 mt-3 footer_haberler">
                             <a href="https://m-budak.github.io/cargurue/haberler.html">
-                            <h5 class="column-title" >Son Haberler</h5>
-                          </a>
+                                <h5 class="column-title">Son Haberler</h5>
+                            </a>
                             <ul class="list-unstyled">
                                 <div class="row">
                                     <div class="col-12">
                                         <li><a href="#link16">2023’te Türkiye’de En Çok Satan Arabalar</a></li>
                                         <li><a href="#link17">2024 Tesla Fiyatları Nasıl?</a></li>
-                                        <li><a href="#link18">İstanbul İzmir Otoyolu Geçiş Ücretleri 2024</a></li>
-                                        <li><a href="#link19">2024’te Türkiye’ye Gelecek Elektrikli Arabalar:...</a></li>
-                                        <li><a href="#link20">Yılın En Heyecan Veren 10 Otomobili (2023)</a></li>
-                                        <li><a href="#link20">Elektrikli Araba Hakkında En Çok Merak Edilenler</a></li>
+                                        <li><a href="#link18">Yeni Yılda Hangi Arabalar Çıkıyor?</a></li>
+                                        <li><a href="#link19">TOGG Yerli Elektrikli Araba</a></li>
+                                        <li><a href="#link15">İkinci El Araba Alırken Dikkat Edilmesi Gerekenler</a></li>
                                     </div>
                                 </div>
                             </ul>
@@ -191,12 +150,6 @@ class MyFooter extends HTMLElement {
                     </div>
                 </div>
             </footer>
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-            <link rel="stylesheet" href="../style.css">
-            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
         `;
     }
 }
